271db61e9a3bc68329dd2cec1c0736c140e7aa9c
MARIOHELIOSIMOES <mariohelio_simoes@hotmail.com>
2021-08-28 14:24:17 -0300

    Update README

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
-# ProjetosPublicos
-Seja bem-vindo!
+# Projetos Publicos
+Seja bem-vindo(a)!
 
-Este repositório é destinado a exposição de alguns trabalhos e projetos que já realizei.
+Este repositório é destinado a exposição de alguns trabalhos e projetos pessoais com o intuito de compartilhar conhecimento e demonstrar minha parte técnica e experiências.
+
+Por meio dos links abaixo é possível navegar pelos diferentes segmentos de conhecimento:
+
+- [Artigos Acadêmicos](https://github.com/MARIOHELIOSIMOES/ProjetosPublicos/tree/main/ARTIGOS%20E%20TCC%20UNIVESP)
+- [Android](https://github.com/MARIOHELIOSIMOES/ProjetosPublicos/tree/main/Android)
+- [Android antigos](https://github.com/MARIOHELIOSIMOES/ProjetosPublicos/tree/main/Android%20Antigos)
+- [C](https://github.com/MARIOHELIOSIMOES/ProjetosPublicos/tree/main/C)
+- [Certificados/Cursos](https://github.com/MARIOHELIOSIMOES/ProjetosPublicos/tree/main/Certificados)
+- [HTML5](https://github.com/MARIOHELIOSIMOES/ProjetosPublicos/tree/main/Curso%20em%20Video%20-%20HTML5/HTML5)
+- [JAVA](https://github.com/MARIOHELIOSIMOES/ProjetosPublicos/tree/main/JAVA)
+- [KOTLIN](https://github.com/MARIOHELIOSIMOES/ProjetosPublicos/tree/main/Kotlin)
+- [LADDER](https://github.com/MARIOHELIOSIMOES/ProjetosPublicos/tree/main/LADDER)
 
 Fique a vontade para explorar.
 
 Até mais,
 
 Mário Hélio Simões
-*************************************************************************************
-Welcome!
-This repository has some projects I have done.
-Explore it as you wish.
+===
 
-See you later,
-Mário Hélio Simões.
---
+++ Semana do Engenheiro Projetista/Projetos.mm
@@ -0,0 +1,54 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1629807172573" ID="ID_1971643136" MODIFIED="1629807194726" TEXT="Projetos">
+<node CREATED="1629808310836" ID="ID_280447400" MODIFIED="1629808326025" POSITION="right" TEXT="1&#xba; Dia ">
+<node CREATED="1629808331405" ID="ID_130250947" MODIFIED="1629808333445" TEXT="https://www.youtube.com/watch?v=Sp2IquCy52I"/>
+<node CREATED="1629807196514" ID="ID_1437580554" MODIFIED="1629808335203" TEXT="1 - Organizar o projeto em pastas"/>
+<node CREATED="1629807211755" ID="ID_947208244" MODIFIED="1629807221832" TEXT="2 - Analisar informa&#xe7;&#xf5;es do projeto">
+<node CREATED="1629807223241" ID="ID_975275850" MODIFIED="1629807228840" TEXT="P&#xe9; direito">
+<node CREATED="1629807228845" ID="ID_1293514851" MODIFIED="1629807236050" TEXT="Altura do parede"/>
+</node>
+<node CREATED="1629807237552" ID="ID_572479327" MODIFIED="1629807244306" TEXT="Tipo de alvenaria">
+<node CREATED="1629807529598" ID="ID_767644797" MODIFIED="1629807532990" TEXT="Drywall"/>
+<node CREATED="1629807533287" ID="ID_269062872" MODIFIED="1629807536195" TEXT="Convencional"/>
+<node CREATED="1629807536505" ID="ID_1163536280" MODIFIED="1629807539438" TEXT="Estrutural"/>
+</node>
+<node CREATED="1629807544365" ID="ID_1413810333" MODIFIED="1629807551737" TEXT="Fa&#xe7;a um tour pela edifica&#xe7;&#xe3;o"/>
+<node CREATED="1629809168284" ID="ID_914864715" MODIFIED="1629809174203" TEXT="Qual o tipo de laje"/>
+</node>
+<node CREATED="1629809302838" ID="ID_621171614" MODIFIED="1629809319812" TEXT="3 - Limpar a planta de arquitetura">
+<node CREATED="1629809459094" ID="ID_1921810224" MODIFIED="1629809493955" TEXT="A planta de deve estar limpa para que os componentes el&#xe9;tricos se sobresaiam"/>
+</node>
+<node CREATED="1629809562501" ID="ID_1809638329" MODIFIED="1629809573444" TEXT="4 - Inser&#xe7;&#xe3;o dos ponto de iluminha&#xe7;&#xe3;o e tomada"/>
+<node CREATED="1629809755627" ID="ID_1352471319" MODIFIED="1629809768258" TEXT="5 - Divis&#xe3;o de circuitos na instala&#xe7;&#xe3;o">
+<node CREATED="1629809897831" ID="ID_170947033" MODIFIED="1629809923493" TEXT="Os circuitos devem ser individualizados pela fun&#xe7;&#xe3;o dos equipamentos de utiliza&#xe7;&#xe3;o que alimentam. "/>
+</node>
+<node CREATED="1629810170349" ID="ID_935338069" MODIFIED="1629810193357" TEXT="6 - Inser&#xe7;&#xe3;o de tubula&#xe7;&#xe3;o e fia&#xe7;&#xe3;o">
+<node CREATED="1629810198047" ID="ID_1180198149" MODIFIED="1629810198047" TEXT=""/>
+</node>
+<node CREATED="1629810235052" ID="ID_1469716169" MODIFIED="1629810246679" TEXT="7 - Elabora&#xe7;&#xe3;o do quadro de cargas">
+<node CREATED="1629810288691" ID="ID_433853914" MODIFIED="1629810304859" TEXT="Quantidade de circuitos"/>
+<node CREATED="1629810305240" ID="ID_1825513733" MODIFIED="1629810315416" TEXT="detalhes de cada circuito"/>
+</node>
+<node CREATED="1629810384739" ID="ID_1721738127" MODIFIED="1629810399758" TEXT="8 - Elabora&#xe7;&#xe3;o do c&#xe1;lculo de demanda e queda de tens&#xe3;o"/>
+<node CREATED="1629810632718" ID="ID_341222242" MODIFIED="1629810646384" TEXT="9 - Balanceamento de fases e dimensionamento final"/>
+<node CREATED="1629810852306" ID="ID_1302976306" MODIFIED="1629810873289" TEXT="10 - Montar o diagrama unifilar"/>
+<node CREATED="1629810947185" ID="ID_1364627913" MODIFIED="1629810957255" TEXT="11 - Inser&#xe7;&#xe3;o de detalhes no projeto"/>
+<node CREATED="1629811165112" ID="ID_1185810301" MODIFIED="1629811174985" TEXT="12 - Montagem da prancha e plotagem"/>
+<node CREATED="1629811378327" FOLDED="true" ID="ID_1699374030" MODIFIED="1629811614552" TEXT="13 - Elabora&#xe7;&#xe3;o de lista de materiais (diferencial)">
+<node CREATED="1629811586030" ID="ID_1075104724" MODIFIED="1629811597133" TEXT="Fotos do produtos"/>
+<node CREATED="1629811597411" ID="ID_658297280" MODIFIED="1629811603506" TEXT="N&#xfa;mero de revis&#xe3;o do projeto"/>
+<node CREATED="1629811603791" ID="ID_1631351069" MODIFIED="1629811610756" TEXT="Detalhe do item"/>
+</node>
+<node CREATED="1629811569152" ID="ID_1731864902" MODIFIED="1629811579152" TEXT="14 - Elabora&#xe7;&#xe3;o do memorial descritivo">
+<node CREATED="1629811623174" ID="ID_1208338756" MODIFIED="1629811642920" TEXT="Descrever como ser&#xe1; a edifica&#xe7;&#xe3;o "/>
+<node CREATED="1629811644510" ID="ID_1142851523" MODIFIED="1629811647709" TEXT="Alimenta&#xe7;&#xe3;o"/>
+<node CREATED="1629811648020" ID="ID_1119059159" MODIFIED="1629811650363" TEXT="Aterramento"/>
+</node>
+<node CREATED="1629811670568" ID="ID_238419600" MODIFIED="1629811677964" TEXT="15 - Elabora&#xe7;&#xe3;o da ART"/>
+<node CREATED="1629812328222" ID="ID_1541845048" MODIFIED="1629812337160" TEXT="@fam.engenhariaeprojetos">
+<node CREATED="1629812338995" ID="ID_947144950" MODIFIED="1629812348080" TEXT="print dos resumos no instagram"/>
+</node>
+</node>
+</node>
+</map>
